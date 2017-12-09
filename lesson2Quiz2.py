f60657dfaacffe3be805e91912531b70d8182ad6
harshad90 <bharshad90@gmail.com>
2017-12-09 17:38:15 +0530

    Solution for lesson-2 Quiz-2

---
+++ lesson2Quiz2.py
@@ -0,0 +1,27 @@
+x = 3.14159      
+
+#ENTER CODE BELOW HERE
+
+y = str(x)
+
+decimal_pos = y.find(".")
+
+
+z = y[decimal_pos+1]
+
+ 
+
+
+print (z);
+if (z>=str(5)):
+
+   
+  a=y[:decimal_pos]
+  print (int(a)+1);
+else:
+
+    
+  b=y[:decimal_pos]
+  print(int(b));
+  
+    
