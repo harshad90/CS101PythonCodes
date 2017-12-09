559aa8aafaa24d974124e51d78303e936910c379
Harshad Bhangale <bharshad90@gmail.com>
2017-12-09 11:22:23 +0530

    Update finalQuiz40.py

--- finalQuiz40.py
+++ finalQuiz40.py
@@ -5,20 +5,25 @@
 # Your code should print http://udacity.com
 # Make sure that if page were changed to
 #page = '<div id="top_bin"><a href="http://udacity.com">Hello world</a>'
-'<div class="udacity float-left"'
+#'<div class="udacity float-left"'
 # that your code still assigns the same value to the variable 'url', 
 # and therefore still prints the same thing.
 # page = contents of a web page
+
 page =('<div id="top_bin"><div id="top_content" class="width960">'
 '<div class="udacity float-left"><a href="http://udacity.com">')
+
 start_link = page.find('<a href=') #finding the position of '<a href' 
 #print start_link
+
 index=page[start_link:].find("http://")
 #finding the position of 'http' using '<a href' index 
 #print index
+
 index2=page[start_link+index:].find('"')
 #finding position of ""(quotes) using both  index positions
 #print index2
+
 url=page[start_link+index:start_link+index+index2]
 #assigning the value to url variable
 print (url);
